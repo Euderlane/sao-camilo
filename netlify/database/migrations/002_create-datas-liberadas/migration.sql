e748587579380ad23bc0569f800bdaf01eb1d285
Euderlane <euderlane@saocamilo.net>
2026-05-16 17:56:46 -0300

    Delete netlify/database/migrations/002_create-datas-liberadas/migration.sql

--- netlify/database/migrations/002_create-datas-liberadas/migration.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS datas_liberadas (
-  data       TEXT PRIMARY KEY,
-  criado_em  TIMESTAMPTZ DEFAULT NOW()
-);
-
-INSERT INTO datas_liberadas (data)
-VALUES ('2026-05-20')
-ON CONFLICT (data) DO NOTHING;
